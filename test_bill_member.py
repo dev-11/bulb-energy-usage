88de69aa10863d8c9d989679695695a55822f48b
Otto <otto@masterbranch.io>
2020-05-08 18:10:23 +0100

    adding extra testcases

--- test_bill_member.py
+++ test_bill_member.py
@@ -5,27 +5,65 @@ from bill_member import calculate_bill
 
 class TestBillMember(unittest.TestCase):
 
-    def test_calculate_bill_for_august(self):
+    def test_calculate_bill_for_ALL_for_august(self):
         amount, kwh = calculate_bill(member_id='member-123',
                                      account_id='ALL',
                                      bill_date='2017-08-31')
         self.assertEqual(amount, 27.57)
         self.assertEqual(kwh, 167)
 
-    def test_calculate_bill_for_first_month(self):
+    def test_calculate_bill_for_ALL_first_month(self):
         amount, kwh = calculate_bill(member_id='member-123',
                                      account_id='ALL',
                                      bill_date='2017-03-28')
         self.assertEqual(amount, 2107.51)
         self.assertEqual(kwh, 17580)
 
-    def test_calculate_bill_for_unknown_month(self):
+    def test_calculate_bill_for_ALL_unknown_month(self):
         amount, kwh = calculate_bill(member_id='member-123',
                                      account_id='ALL',
                                      bill_date='2022-12-31')
         self.assertEqual(amount, 7.61)
         self.assertEqual(kwh, 0)
 
+    def test_calculate_bill_for_account_abc_for_august(self):
+        amount, kwh = calculate_bill(member_id='member-123',
+                                     account_id='account-abc',
+                                     bill_date='2017-08-31')
+        self.assertEqual(amount, 27.57)
+        self.assertEqual(kwh, 167)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_calculate_bill_for_account_abc_first_month(self):
+        amount, kwh = calculate_bill(member_id='member-123',
+                                     account_id='ALL',
+                                     bill_date='2017-03-28')
+        self.assertEqual(amount, 2107.51)
+        self.assertEqual(kwh, 17580)
+
+    def test_calculate_bill_for_account_abc_unknown_month(self):
+        amount, kwh = calculate_bill(member_id='member-123',
+                                     account_id='account-abc',
+                                     bill_date='2022-12-31')
+        self.assertEqual(amount, 7.61)
+        self.assertEqual(kwh, 0)
+
+    def test_calculate_bill_for_invalid_account_for_august(self):
+        amount, kwh = calculate_bill(member_id='member-123',
+                                     account_id='account-abcd',
+                                     bill_date='2017-08-31')
+        self.assertEqual(amount, 0)
+        self.assertEqual(kwh, 0)
+
+    def test_calculate_bill_for_invalid_account_first_month(self):
+        amount, kwh = calculate_bill(member_id='member-123',
+                                     account_id='account-abcd',
+                                     bill_date='2017-03-28')
+        self.assertEqual(amount, 0)
+        self.assertEqual(kwh, 0)
+
+    def test_calculate_bill_for_invalid_account_for_unknown_month(self):
+        amount, kwh = calculate_bill(member_id='member-123',
+                                     account_id='account-abcd',
+                                     bill_date='2022-12-31')
+        self.assertEqual(amount, 0)
+        self.assertEqual(kwh, 0)
